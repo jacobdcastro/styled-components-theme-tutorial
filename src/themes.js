17ac82df0a3f6f8f56c4a83afff6000b1803f84b
Jacob D. Castro <jdcastro.business@gmail.com>
2019-08-23 22:37:18 -0700

    establish themes

---
+++ src/themes.js
@@ -0,0 +1,13 @@
+const darkTheme = {
+  bgColor: '#282c34',
+  textColor: 'white',
+  linkColor: '#61dafb',
+};
+
+const lightTheme = {
+  bgColor: '#e8ecf0',
+  textColor: '#001c38',
+  linkColor: '#5dd2f2',
+};
+
+export { darkTheme, lightTheme };
